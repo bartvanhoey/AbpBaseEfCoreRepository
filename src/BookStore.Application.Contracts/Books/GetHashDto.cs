5a5202c12c0973f5b55cc3b22c13f64364903040
ekapituro <ekapituro@gmail.com>
2024-02-13 21:25:34 +0100

    Update

--- src/BookStore.Application.Contracts/Books/GetHashDto.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BookStore.Books
-{
-    public class GetHashDto
-    {
-        public Guid? TenantId { get; set; }
-    }
-}
--- src/BookStore.Blazor/Pages/Index.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookStore.Blazor.Pages
-{
-    public partial class Index
-    {
-
-    }
-}
